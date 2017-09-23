442df8b65219914c3e82fd7c872359b564e001ce
student13 <student13@itpark.ru>
2017-09-23 20:51:22 +0300

    start task1 and task2

---
+++ Arrays/src/ru/itpark/Main.java
@@ -0,0 +1,20 @@
+package ru.itpark;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите число n");
+            int n;
+            n = scanner.nextInt();
+            int[] nums= new int[n];
+            for (int i=0;i<nums.length;i++);
+            nums[n]= scanner.nextInt();
+            if 
+        }
+    };
+}
