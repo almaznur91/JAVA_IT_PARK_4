1a4f13f29581085850b35d15ecc32e4c4d61dd97
student13 <student13@itpark.ru>
2017-09-28 20:03:45 +0300

    add tusk3

---
+++ tusk3/src/ru/itpark/Main.java
@@ -0,0 +1,34 @@
+
+import static java.lang.Math.*;
+    public static void main(String[] args) {
+	// write your code here
+
+
+        public class Main {
+
+            public static double f(double x) {
+                return sin(x);
+            }
+
+            public static double integrate(int a, int b, int n) {
+                // рассчитать ширину разбиения
+                double h = abs(a - b) / (double)(n);
+                for (double x=a;x<=b,)
+                return 0;
+            }
+
+            public static int sum(double a, double b) {
+                int result = a + b;
+                return result;
+            }
+
+            public static void main(String[] args) {
+                int sumResult = sum(5, 6);
+            }
+
+
+        }
+
+
+    }
+
