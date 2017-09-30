cbaf846d99988a64cb11f0ca6e288d642ac29dc5
almaznur91 <almazic91@gmail.com>
2017-09-30 16:11:43 +0300

    homework_3

--- Homeworks/Task2/src/ru/itpark/Main.java
+++ Homeworks/Task2/src/ru/itpark/Main.java
@@ -1,37 +1,88 @@
-package com.company;
-
 import java.util.Scanner;
 
 public class Main {
 
+    public static void showArray(int count, int[] array) {
+        for (int i = 0; i < count; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+    public static int addToBegin(int count1, int[] array) {
+        System.out.println("Введите число для добавления в начало");
+        for (int a = count1; a > 0; a = a - 1) {
+            array[a] = array[a-1];
+        }
+        array[0] = new Scanner(System.in).nextInt();
+        count1++;
+        return count1;
+    }
+
+
+    public static int addToFinish(int count, int[] array) {
+        System.out.println("Введите число для добавления в конец");
+        array[count] = new Scanner(System.in).nextInt();
+        count++;
+        return count;
+    }
+
+
+    public static int Del(int count1, int[] array) {
+        System.out.println("Введите число для удаления по позиции");
+        int n = new Scanner(System.in).nextInt();
+        if (n >= count1) {
+            System.out.println("Введите верное значение");
+        } else {
+            for (int a = n; a < count1; a = a + 1)
+                array[a] = array[a + 1];
+        }
+        count1--;
+        return count1;
+    }
+
+
+    public static void regularize(int[] array, int count) {
+        int min;
+        int poryadkovyNomerMinZnach;
+        int temp;
+        for (int n = 0; n < count; n++) {
+            min = array[n];
+            poryadkovyNomerMinZnach = n;
+            for (int i = n + 1; i < count; i++) {
+                if (min > array[i]) {
+                    min = array[i];
+                    poryadkovyNomerMinZnach = i;
+                }
+            }
+            temp = array[poryadkovyNomerMinZnach];
+            array[poryadkovyNomerMinZnach] = array[n];
+            array[n] = temp;
+        }
+    }
 
     public static void main(String[] args) {
         // write your code here
         Scanner scanner = new Scanner(System.in);
-        int choise;
 
         int[] array = new int[100];
         int count = 0;
+        int choise;
 
+        System.out.println("\nМЕНЮ:\n" +
+                "1. Показать массив - выводит только count-элементов\n" +
+                "2. Добавить в начало\n" +
+                "3. Добавить в конец\n" +
+                "4. Удалить по позиции\n" +
+                "5. Удалить по позиции\n" +
+                "6. Выход");
 
         while (true) {
-
-            System.out.println("\nМЕНЮ:\n" +
-                    "1. Показать массив - выводит только count-элементов\n" +
-                    "2. Добавить в начало\n" +
-                    "3. Добавить в конец\n" +
-                    "4. Удалить по позиции\n" +
-                    "5. Выход");
-
             choise = scanner.nextInt();
-
             switch (choise) {
                 case 1:
-
-                    for (int i = 0; i < count; i++) {
-                        System.out.print(array[i]);
-                    }
-
+                    showArray(count, array);
                     break;
                 case 2:
 
@@ -39,61 +90,28 @@ public class Main {
                         System.out.println("Заполнено");
                         break;
                     }
-
-                    System.out.println("Введите число для добавления в начало");
-                    for (int a = count, b = count - 1; b >= 0; a = a - 1, b = b - 1) {
-                        array[a] = array[b];
-                    }
-                    array[0] = scanner.nextInt();
-                    count++;
-
+                    count = addToBegin(count, array);
                     break;
-
-
                 case 3:
                     if (count == 100) {
                         System.out.println("Заполнено");
                         break;
                     }
-                    System.out.println("Введите число для добавления в конец");
-                    array[count] = scanner.nextInt();
-                    count++;
-
+                    count = addToFinish(count, array);
                     break;
-
-
                 case 4:
-                    System.out.println("Введите число для удаления по позиции");
-                    int n = scanner.nextInt();
-
-
-                    if (n >= count) {
-                        System.out.println("Введите верное значение");
-                        break;
-                    }
-
-
-                    for (int a = n; a < count; a = a + 1)
-                        array[a] = array[a + 1];
-                    count--;
-
-
+                    count = Del(count, array);
                     break;
-
-
                 case 5:
-                    System.exit(0);
-
+                    regularize(array, count);
                     break;
-
+                case 6:
+                    System.exit(0);
+                    break;
                 default:
                     System.out.println("Введите верное значение");
-
                     break;
             }
-
-
         }
     }
-
 }
