7bc78b57d36dbc3cb09c27e3975f26df4a5bae72
almaznur91 <32224755+almaznur91@users.noreply.github.com>
2017-10-11 19:08:37 +0300

    fix error in square(Task6)

--- Homeworks/Task6/src/com/company/Square.java
+++ Homeworks/Task6/src/com/company/Square.java
@@ -3,12 +3,12 @@ package com.company;
  class Square extends Rectangle {
 
 
-    public Square(int side1, int side2) {
-        super(side1, side2);
+    public Square(int side1) {
+        super(side1, side1);
     }
 
     public void area() {
-        System.out.println("Площадь квадрата = " + (side1 * side2));
+        System.out.println("Площадь квадрата = " + (side1 * side1));
     }
 
 }
